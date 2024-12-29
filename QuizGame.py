de079bfbf4a4bfc40b3ef1e3c46bc40bbf9cb256
AryanNagori1405 <161335137+AryanNagori1405@users.noreply.github.com>
2024-12-29 23:05:54 +0530

    QuizGame.py

--- QuizGame.py
+++ QuizGame.py
@@ -1,3 +1,4 @@
+# These are the set of questions and options displayed in front of the user
 questions = [
     {
         'question' : 'Which of the following is a mutable data type in Python?',
@@ -33,17 +34,18 @@ questions = [
 ]
 
 def QuizGame(questions):
-    score = 0
-    for i, ques in enumerate(questions, start=1):
+    print("Welcome to Quiz Game")
+    score = 0    # Here score is initialized to zero so later it can be incremented
+    for i, ques in enumerate(questions, start=1):    # This 'for' loop will display the question 
         print(f"{i}.{ques['question']}")
-        for option in ques['options']:
+        for option in ques['options']:     # This 'for' loop will display the options for that question 
             print(option)
         user_input = input("Enter the answer (a, b, c or d): ").lower()
-        if user_input == ques['actual_answer']:
-            print("Correct, Hooray!!!")
-            score += 1
+        if user_input == ques['actual_answer']:    # This 'if' will check whether the input given by user is same as the actual answer
+            print("Correct, Hooray!!!")    # If Yes, then this will be printed
+            score += 1    # And the score will be incremented by 1
         else:
-            print(f"Wrong answer, The correct answer was {ques['actual_answer']}")
-        print(f"Your score is {score}\n")
+            print(f"Wrong answer, The correct answer was {ques['actual_answer']}")    # If No, then this will be printed
+        print(f"Your score is {score}\n")    # At last the total score will be printed
 
 QuizGame(questions)
